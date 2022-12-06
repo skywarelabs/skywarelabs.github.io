a4b27ff56afa8c703918877660e06dfa1f6158dc
skywarelabs <109983202+skywarelabs@users.noreply.github.com>
2022-12-06 06:02:19 +0200

    Delete All

--- navigationCommandAic.js
+++
@@ -1,4 +0,0 @@
-	var lang1RootHeaderAic = new Array('HEADERLang1','HEADER1Lang1','HEADER2Lang1');
-	var lang2RootHeaderAic = new Array();
-	var menuItemSectionAicLang1 = new Array();
-	var menuItemSectionAicLang2 = new Array();
