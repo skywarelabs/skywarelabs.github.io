e81eeb33c55a985868bc427e92de4e4bcb975eeb
skywarelabs <109983202+skywarelabs@users.noreply.github.com>
2023-06-15 13:51:07 -0400

    June 2023

---
+++ AIRAC AMDT 01-2023_2023_06_15/navigationCommand.js
@@ -0,0 +1,15 @@
+var currentLang;
+var lang1='en-GB';
+var HTML_FOLDER = '/eAIP';
+	var PDF_FOLDER = '/documents/PDF';
+	var SUP_FOLDER='/eSUP';
+	var AIC_FOLDER='/eAIC';
+	var coverPagePDFFile='/FL-Cover Page.pdf';
+	var lang2="";
+var lang1RootHeader = new Array("GEN_HEADERLang1","ENR_HEADERLang1","AD_HEADERLang1");
+var lang2RootHeader = new Array();
+	var lang1RootHeaderAMDT = new Array("MISC_HEADERLang1","ENR Enroute_HEADERLang1","AD Aerodrome_HEADERLang1");
+	var lang2RootHeaderAMDT = new Array("MISC_HEADERLang2","ENR Enroute_HEADERLang2","AD Aerodrome_HEADERLang2");
+
+var folderHelpNational='help_national/';
+var folderHelpInterNational='';
---
+++ AIRAC AMDT 01-2023_2023_06_15/navigationCommandSup.js
@@ -0,0 +1,4 @@
+	var lang1RootHeaderSup = new Array('HEADERLang1','HEADER1Lang1','HEADER2Lang1');
+	var lang2RootHeaderSup = new Array();
+	var menuItemSectionSupLang1 = new Array();
+	var menuItemSectionSupLang2 = new Array();
